776f46e5a9849a01c15f50bb2fbb69b7a1c78e56
Gibbyace <35547104+Gibbyace@users.noreply.github.com>
2022-06-03 10:47:27 +0200

    Startscreen funktioniert jetzt, ruft aber nichts auf

--- startscreen.cpp
+++ startscreen.cpp
@@ -5,16 +5,33 @@ StartScreen::StartScreen(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::StartScreen)
 {
+
     ui->setupUi(this);
+    QWidget::setStyleSheet(("Background-color: black;"));
+
+    QPixmap pixmapTarget = QPixmap("../pg2_Di45y-TeamA-Herrmann_Kotwal/textures/startscreen.png");
+    ui->label->setPixmap(pixmapTarget);
+
+    QPixmap PixmapTarget3 = QPixmap("../pg2_Di45y-TeamA-Herrmann_Kotwal/textures/bloody_frame.png");
+    ui->label_2->setPixmap(PixmapTarget3);
+
+    QIcon pixmapTarget2 = QPixmap("../pg2_Di45y-TeamA-Herrmann_Kotwal/textures/new_game_button.png");
+    ui->pushButton->setIcon(pixmapTarget2);
+
+
+
+
 }
 
 StartScreen::~StartScreen()
 {
     delete ui;
+
 }
 
 void StartScreen::on_pushButton_clicked()
 {
 
+    ui->pushButton->hide();
+    this->hide();
 }
-

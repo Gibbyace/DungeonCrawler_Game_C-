8806f379aeb96fc8eb592bf0ca977c444ff8333c
Leo Herrmann <leo.herrmann@stud.h-da.de>
2022-06-05 10:18:02 +0200

    include <typeinfo>

--- pit.cpp
+++ pit.cpp
@@ -1,22 +1,23 @@
 ﻿#include "pit.h"
 #include "ramp.h"
+#include <typeinfo>
 
 Pit::Pit(int row, int column): Tile(row, column)
 {
     this->texture = "_";
 
 }
+
 Tile *Pit::onEnter(Tile *fromTile, Character *who)
 {
     return this;
 }
 
-Tile *Pit::onLeave(Tile *destTile, Character *who) //Müssen *destile implementieren und dann mit typeid bestimmen
+Tile *Pit::onLeave(Tile *destTile, Character *who)
 {
     if ((typeid(*destTile) == typeid(Pit)) or (typeid(*destTile) == typeid(Ramp))){
         return this;
     }
-    else
-        return nullptr;
 
+    return nullptr;
 }

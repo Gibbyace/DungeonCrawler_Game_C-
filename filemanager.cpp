4aac94db28b3d27ecb837132922ffebbca5eb04f
Leo Herrmann <leo.herrmann@stud.h-da.de>
2022-07-04 20:40:08 +0200

    levels.json wird korrekt eingelesen. Als nächstes steht die Interpretation als LevelList an

--- filemanager.cpp
+++ filemanager.cpp
@@ -1,4 +1,5 @@
 #include "filemanager.h"
+#include <fstream>
 
 Filemanager::Filemanager()
 {
@@ -7,5 +8,22 @@ Filemanager::Filemanager()
 
 nlohmann::json Filemanager::loadLevels()
 {
+    std::ifstream file("../pg2_Di45y-TeamA-Herrmann_Kotwal/levels.json");
 
+    nlohmann::json levels;
+
+    file >> levels;
+
+    return levels;
+}
+
+LevelList *Filemanager::createLevelListFromJSON(nlohmann::json json)
+{
+    LevelList* levellist = new LevelList();
+
+    for (auto level : json["levellist"]) {
+        cout << level["id"] << endl;
+    }
+
+    return levellist;
 }
--- main.cpp
+++ main.cpp
@@ -2,14 +2,20 @@
 #include "dungeoncrawler.h"
 #include <QApplication>
 
+#include "filemanager.h"
+
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-    QApplication application(argc, argv);
+    Filemanager filemanager = Filemanager();
+    nlohmann::json json = filemanager.loadLevels();
+    filemanager.createLevelListFromJSON(json);
+
+    /*QApplication application(argc, argv);
 
     DungeonCrawler Game1 = DungeonCrawler();
-    Game1.play();
+    Game1.play();*/
 
     return 0;
 }
